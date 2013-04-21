32f342d156899f5057b97ea30dd4b1918fd9f1d8
Hugo Perez <perezalvarezhi@gmail.com>
2013-04-20 19:11:27 -0500

    Background Added

--- GetWebPage/src/com/msi/getwebpage/GetWebPage.java
+++ GetWebPage/src/com/msi/getwebpage/GetWebPage.java
@@ -8,10 +8,12 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.view.View;
 // used for passing data 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 // used for connectivity
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -47,32 +49,59 @@ public class GetWebPage extends Activity {
                 super.handleMessage(msg);
             }
         };
+        
         final Button button = (Button) findViewById(R.id.ButtonGo);
         button.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-            	try	{
-            		tView.setText("");
-                // Perform action on click
-                	URL url = new URL(eText.getText().toString());
-                    URLConnection conn = url.openConnection();
-                    // Get the response
-                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    String line = "";
-                    while ((line = rd.readLine()) != null) {
-                		Message lmsg;
-                        lmsg = new Message();
-                        lmsg.obj = line;
-                        lmsg.what = 0;
-                        GetWebPage.this.h.sendMessage(lmsg);
-                        
-                    }
-                    SmsManager sm = SmsManager.getDefault();
-                    //sm.sendDataMessage("test",null,(short)5556,"hello there".getBytes(),null,null);
-                    sm.sendTextMessage("test",null,"hello there",null,null);
-            	}
-            	catch (Exception e)	{
-            	}
+        
+        public void onClick(View v) {
+        	tView.setText("");
+        	new DownloadWebpageTask().execute(eText.getText().toString());
+        	
             }
-        });        
+        });
+        
+        
+        
+        
+        
+        
     }
+	
+	
+	
+	private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+              
+            
+            try {     		
+	        	URL url = new URL(urls[0]);
+	            URLConnection conn = url.openConnection();
+	            // Get the response
+	            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	            String line = "";
+	            while ((line = rd.readLine()) != null) {
+	        		Message lmsg;
+	                lmsg = new Message();
+	                if ( line.substring(0, 2) == "<td"  ){
+	                	lmsg.obj = line;
+	                    lmsg.what = 0;
+	                    GetWebPage.this.h.sendMessage(lmsg);
+	                }
+	                
+	            }
+	            SmsManager sm = SmsManager.getDefault();
+	            //sm.sendDataMessage("test",null,(short)5556,"hello there".getBytes(),null,null);
+	            sm.sendTextMessage("test",null,"hello there",null,null);
+	    	
+            } catch (IOException e) {
+            	System.out.print( "Unable to retrieve web page. URL may be invalid.");
+            }
+            return "Success";
+            
+        }
+        
+    }
+	
+	
 }
